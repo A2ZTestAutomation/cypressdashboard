327a862dc3d6cdba0c85898614100eeadd93b6f1
anandhi-k <anandhi.velavan@gmail.com>
2021-02-21 12:10:06 +0530

    With Nodemodules

---
+++ cypress/integration/examples/sample.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+
+describe('Test suite', () => {
+
+    it('Test case 1 positive', () => {
+    cy.visit('https://www.nopcommerce.com/en/demo')
+    //.title().should('be.eq', 'Store Demo - nopCommerce')
+    
+    //cy.pause()
+    
+   // cy.get("a[href*='/en']").should('have.length', 77)
+    //cy.get(".top-menu.desktop > :nth-child(1)").invoke('show')
+    // cy.contains('PRODUCT').click
+    // cy.contains('Features').click({force:true})
+   // cy.contains('Features').should('be.visible').click()
+   // cy.get("a[href*='/en/features']").click()
+   // cy.url().should("include", "/features")
+    })
+    })
+    
