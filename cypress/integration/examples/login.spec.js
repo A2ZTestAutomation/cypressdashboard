ff9c9c25646a840470b9e9e9ff98a1e1778bdb6d
A2ZTestAutomation <corpdevops@gmail.com>
2022-02-02 21:46:07 +0530

    Login spec modified

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -9,16 +9,16 @@ describe("Testing login functionality",
     // }, 
     () => {
     
-    it("Login success", () => {
+    it.only("Login success", () => {
         cy.visit("https://the-internet.herokuapp.com/login")
         cy.get("#username").type("tomsmith")
         cy.get("#password").type("SuperSecretPassword!")
         cy.get(".radius").click()
-      //  cy.url().should("include", "secure")
+       cy.url().should("include", "secure")
         //To make it fail
-        cy.url().should("include", "Login success")
+        // cy.url().should("include", "Login success")
     })
-    it.only("Login Failure-Username", () => {
+    it("Login Failure-Username", () => {
         cy.visit("https://the-internet.herokuapp.com/login")
         cy.get("#username").type("tom")
         cy.get("#password").type("SuperSecretPassword!")
